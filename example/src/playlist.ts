350ff81ea987abd994429f6a9856f9ee4a22303b
Brad Flood <brad@trusst.ai>
2025-03-25 12:32:28 +1000

    feat: full sound files + renamed artwork

--- example/src/playlist.ts
+++ example/src/playlist.ts
@@ -2,15 +2,21 @@ import type { AudioProTrack } from 'react-native-audio-pro';
 
 export const playlist: AudioProTrack[] = [
   {
-    url: 'https://rnap.dev/soundhelix-song-1-tschurger.m4a',
+    url: 'https://rnap.dev/audio-soundhelix-song-1-tschurger.m4a',
     title: 'Soundhelix Song 1',
     artist: 'T. Schurger',
-    artwork: 'https://rnap.dev/usgs-bAji8qv_LlY-unsplash.jpg',
+    artwork: 'https://rnap.dev/artwork-usgs-bAji8qv_LlY-unsplash.jpg',
   },
   {
-    url: 'https://rnap.dev/soundhelix-song-5-tschurger.m4a',
-    title: 'Soundhelix Song 5',
+    url: 'https://rnap.dev/audio-soundhelix-song-2-tschurger.m4a',
+    title: 'Soundhelix Song 2',
     artist: 'T. Schurger',
-    artwork: 'https://rnap.dev/usgs-PgL1p8TBGNQ-unsplash.jpg',
+    artwork: 'https://rnap.dev/artwork-usgs-PgL1p8TBGNQ-unsplash.jpg',
+  },
+  {
+    url: 'https://rnap.dev/audio-soundhelix-song-9-tschurger.m4a',
+    title: 'Soundhelix Song 9',
+    artist: 'T. Schurger',
+    artwork: 'https://rnap.dev/artwork-usgs-8tfu4320oxI-unsplash.jpg',
   },
 ];
