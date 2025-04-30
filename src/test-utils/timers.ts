fc7ba4a8333dfaa3b82f1a8f4f127c2949ec751f
Brad Flood <brad@trusst.ai>
2025-04-30 16:55:03 +1000

    chore: reset ts tests

--- src/test-utils/timers.ts
+++
@@ -1,9 +0,0 @@
-export const mockTimers = {
-  setTimeout: jest.fn(),
-  setInterval: jest.fn(),
-  clearTimeout: jest.fn(),
-  clearInterval: jest.fn(),
-};
-
-// Export for direct use in tests
-export default mockTimers; 
